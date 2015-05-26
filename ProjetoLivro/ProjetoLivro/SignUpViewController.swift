2f423f99c9ced50e9906f6ea16f365652edf6775
Bruno Pereira <brunopereiras@icloud.com>
2015-05-26 18:51:01 -0300

    Criado parte do storyboard e suas respectivas view controllers

---
+++ ProjetoLivro/ProjetoLivro/SignUpViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SignUpViewController.swift
+//  ProjetoLivro
+//
+//  Created by Bruno Pereira dos Santos on 5/26/15.
+//  Copyright (c) 2015 ABCD. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBOutlet weak var imageField: UIImageView!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordConfirmationField: UITextField!
+
+    @IBAction func signUp(sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
